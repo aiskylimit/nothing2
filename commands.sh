cf37e409dd62d2018f441edc5ae5542f18f6fa98
aiskylimit <nguyenthienhuu88bk@gmail.com>
2026-07-25 13:00:13 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,20 +1,20 @@
-#0
+#1
 #main
 #v1
 
 
-# wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
-# sudo dpkg -i cuda-keyring_1.1-1_all.deb
-# sudo apt update
-# sudo apt-get install -y cuda-toolkit-13-0
-# echo 'export PATH=/usr/local/cuda/bin:$PATH' >> ~/.bashrc
-# echo 'export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
-# source ~/.bashrc
-# bash install_miniconda.sh
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
+sudo apt update
+sudo apt-get install -y cuda-toolkit-13-0
+echo 'export PATH=/usr/local/cuda/bin:$PATH' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
+source ~/.bashrc
+bash install_miniconda.sh
 
-# cd gpu_burn
-# make CUDAPATH=/usr/local/cuda-13.0
-# ./gpu_burn 36000000000
+cd gpu_burn
+make CUDAPATH=/usr/local/cuda-13.0
+./gpu_burn 36000000000
 
 # kill -9 $(nvidia-smi --query-compute-apps=pid --format=csv,noheader)
 
