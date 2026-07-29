0bae184152031624c6978c58e621a697bff88a45
bachthetrollface <bachthetrollface@gmail.com>
2026-07-30 00:02:07 +0700

    update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,5 +1,5 @@
-#1 +120
-#regkd
+#1 +120+a
+#contra
 #v1
 
 #2 -f-/home/ubuntu/aiskylimit_nothing2/anti-sampling/results_yaml/ +a
@@ -59,8 +59,8 @@ export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 # cd contra-only
 # bash ./project_commands.sh
 
-# cd contra-velocity-field-design
-# bash ./project_commands.sh
-
-cd nothing2/regkd-vlm2vec
+cd contra-velocity-field-design
 bash ./project_commands.sh
+
+# cd nothing2/regkd-vlm2vec
+# bash ./project_commands.sh
