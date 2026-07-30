2a269c1bc68fe6341830fdf12c23f8354f2f43d2
aiskylimit <nguyenthienhuu88bk@gmail.com>
2026-07-30 09:44:22 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
-#2
+#1 +10
 #regkd
 #v1
 
@@ -63,9 +63,17 @@ export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 # cd contra-analysis
 # bash ./project_commands.sh
 
+conda env list
 
 # conda create -n python311 python=3.11 -y
 conda activate python311
+
+echo "conda"
+echo $CONDA_DEFAULT_ENV
+echo "python"
+which python
+echo "python ver"
+python -V
 
 cd regkd-vlm2vec
 bash ./project_commands.sh
