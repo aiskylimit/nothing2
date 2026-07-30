9c9ccb004f803689d797a3cd221800deb71e54f8
aiskylimit <nguyenthienhuu88bk@gmail.com>
2026-07-30 22:17:56 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
-#2
+#1 +10
 #test
 #v1
 
@@ -16,9 +16,9 @@
 # source ~/.bashrc
 # bash install_miniconda.sh
 
-cd gpu_burn
-make CUDAPATH=/usr/local/cuda-13.0
-./gpu_burn 36000000000
+# cd gpu_burn
+# make CUDAPATH=/usr/local/cuda-13.0
+# ./gpu_burn 36000000000
 
 
 # kill -9 $(nvidia-smi --query-compute-apps=pid --format=csv,noheader)
