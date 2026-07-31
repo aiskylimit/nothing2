67b794914e21df01af6b83b07890a9e8e9a6d5c1
aiskylimit <nguyenthienhuu88bk@gmail.com>
2026-07-31 21:25:17 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
-#2 -f-/home/ubuntu/aiskylimit_nothing2/text2sql_distillation_draft/run_logs/qwen2.5/20260731_085636/ +a
+#1 +10
 #sql
 #v1
 
@@ -21,8 +21,8 @@
 # ./gpu_burn 36000000000
 
 
-kill -9 $(nvidia-smi --query-compute-apps=pid --format=csv,noheader)
-sleep 5
+# kill -9 $(nvidia-smi --query-compute-apps=pid --format=csv,noheader)
+# sleep 5
 nvidia-smi
 
 # sudo systemctl stop nvidia-fabricmanager
@@ -83,6 +83,6 @@ export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 # cd regkd-vlm2vec
 # bash ./project_commands.sh
 
-cd ./text2sql_distillation_draft
-QWEN25_GPU_LIST=0,1,2,3,4,5,6,7 GPUS_PER_JOB=2 RUN_MODE=parallel \
-BATCH_SIZE=4 GRAD_ACC=4 bash project_commands_qwen2.5.sh
+# cd ./text2sql_distillation_draft
+# QWEN25_GPU_LIST=0,1,2,3,4,5,6,7 GPUS_PER_JOB=2 RUN_MODE=parallel \
+# BATCH_SIZE=4 GRAD_ACC=4 bash project_commands_qwen2.5.sh
